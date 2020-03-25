6a0bd54bd509172b7b01ab8c2dde33ab6cc85499
Juan Alcántara <j.alejandrominaya@gmail.com>
2020-03-25 14:01:07 -0600

    Create prelim layout for Pokemon Detail

---
+++ app/src/main/java/com/itesm/pokedex/activities/ActivityPokemonDetail.kt
@@ -0,0 +1,13 @@
+package com.itesm.pokedex.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.itesm.pokedex.R
+
+class ActivityPokemonDetail : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokemon_detail)
+    }
+}
